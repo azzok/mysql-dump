20b336ecfa96545bca911efb37c1475639700059
azzok <azzok17@gmail.com>
2020-02-14 12:54:58 +0530

    Update shell script for export mysql database table with custom where condition

--- Shell_Script/exportMysqlDB.sh
+++ Shell_Script/exportMysqlDB.sh
@@ -8,5 +8,8 @@ mysqldump -uUsername -pPassword database > database.sql
 #exportFullDB & Ignore some tables
 mysqldump -uUsername -pPassword database --ignore-table=database.table1 --ignore-table=database.table2 > database.sql
 
-#exportParticular DB
-mysqldump -uUsername -pPassword database -t --skip-lock-tables wpzendo_20_postmeta > database.sql
+#exportParticular Table
+mysqldump -uUsername -pPassword database -t --skip-lock-tables tableName > tableName.sql
+
+#exportParticular Table with Select Query
+mysqldump -uUsername -pPassword database tableName --where="1=1 limit 100" > wpPostCustomSelectQuery.sql
